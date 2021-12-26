01dc86b99e27746719b1ec5ce9861596d963cc8c
Dieter Beckers <destroyer01@hotmail.com>
2021-12-26 21:47:13 +0100

    Update TrippExamen.java
    put LCD stuff back

--- src/trippexamen/TrippExamen.java
+++ src/trippexamen/TrippExamen.java
@@ -5,15 +5,15 @@ import lcd.Handler;
 public class TrippExamen {
 
     public static void main(String[] args) throws InterruptedException {
-        //Handler lcd_gpio_Handler = new Handler("v1.0");
-        //lcd_gpio_Handler.writeWelcome();
+        Handler lcd_gpio_Handler = new Handler("v1.0");
+        lcd_gpio_Handler.writeWelcome();
         String text;
         
         int i = 20;
         while (i >0) {
             text = "Test " + i;
             System.out.println(text);
-            //lcd_gpio_Handler.writeLineWithDate(text);
+            lcd_gpio_Handler.writeLineWithDate(text);
             i -= 1;
         }
         Thread.sleep(4000);
